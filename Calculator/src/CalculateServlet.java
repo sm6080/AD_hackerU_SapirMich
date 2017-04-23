84e0f4e470fb8127012b712c35b66052bf25ee55
sm6080 <sapirmichaeli1@gmail.com>
2017-04-23 10:16:21 +0300

    23

--- Calculator/src/CalculateServlet.java
+++ Calculator/src/CalculateServlet.java
@@ -13,54 +13,55 @@ public class CalculateServlet extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        String query=request.getQueryString();
-        Map<String,String> qs=new HashMap<>();
-        if (query!=null){
-            String[] keyValues=query.split("&");
-            for (String keyValue:keyValues   ) {
+        String queryString = request.getQueryString();
+        Map<String, String> qs = new HashMap<>();
+        if(queryString != null){
+            String[] keyValues = queryString.split("&");
+            for(String keyValue : keyValues){
                 String[] keyValuePair = keyValue.split("=");
-                if (keyValuePair.length != 2)
+                if(keyValuePair.length != 2)
                     continue;
-                qs.put(keyValuePair[0],keyValuePair[1]);
+                qs.put(keyValuePair[0], keyValuePair[1]);
             }
         }
-        String action=qs.get("action");
-        if (action==null)
+        String action = qs.get("action");
+        if(action == null)
             return;
-        String operand1String=qs.get("operand1");
-        String operand2String=qs.get("operand2");
-        if (operand1String==null||operand2String==null)
+        String operand1String = qs.get("operand1");
+        String operand2String = qs.get("operand2");
+        if(operand1String == null || operand2String == null)
             return;
-        int operand1;
-        int operand2;
-        try {
+        int operand1 = 0;
+        int operand2 = 0;
+        try{
             operand1 = Integer.valueOf(operand1String);
             operand2 = Integer.valueOf(operand2String);
         }catch (Exception ex){
             return;
         }
-        String result;
-        switch (action) {
+        String result = "";
+        switch (action){
             case "add":
                 result = String.valueOf(operand1 + operand2);
                 break;
-            case "substract":
+            case "subtract":
                 result = String.valueOf(operand1 - operand2);
                 break;
             case "multiply":
                 result = String.valueOf(operand1 * operand2);
                 break;
             case "divide":
-                if (operand2 == 0) {
-                    response.getWriter().write("Division by zero ");
+                if(operand2 == 0){
+                    response.getWriter().write("division by zero error");
                     return;
                 }
                 result = String.valueOf(operand1 / operand2);
                 break;
             default:
-                break;
+                return;
         }
-        response.getWriter().write("");
-
+        response.getWriter().write(result);
     }
+
+
 }
